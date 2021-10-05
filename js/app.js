e4e873727de5175cfbe69b778afa1d86b1b12cb8
razz <razzfit@mail.ru>
2021-10-06 02:06:34 +0300

    added modal window

--- js/app.js
+++ js/app.js
@@ -17,3 +17,36 @@ tabs.forEach(function (item, i) {
     tabsContents[i].classList.add('tabcontent_active');
   });
 });
+
+/// Modal windows
+
+const modal = document.querySelector('.modal');
+const btn = document.querySelectorAll('[data-call-btn]');
+const closeBtn = document.querySelector('.modal__close');
+
+btn.forEach(function(item) {
+  item.addEventListener('click', function() {
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+  });
+});
+
+closeBtn.addEventListener('click', hideModal);
+modal.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    hideModal();
+  }
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.code === 'Escape') {
+    hideModal();
+  }
+});
+
+function hideModal() {
+  modal.classList.add('hide');
+  document.body.style.overflow = '';
+}
+
+
